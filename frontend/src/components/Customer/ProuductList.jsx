53c4b0a81b839871ba9d632a3aef7e85fe343d49
nkrisnaraj <nkrisnaraj007@gmail.com>
2024-06-23 16:24:22 +0530

    nodified

--- frontend/src/components/Customer/ProuductList.jsx
+++ frontend/src/components/Customer/ProuductList.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 function ProuductList() {
+  const [product,setProduct] = useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:5174/backend/api/connection.php')
+    .then((res)=>{
+      setProduct(res.data);
+    });
+  },[]);
+
   return (
     <div className='card w-full d-flex'>
         <div className='card-header align-items-center justify-content-center d-flex'>
@@ -9,9 +18,18 @@ function ProuductList() {
             </div>
         </div>
         <div className="card-body">
-            <table className="table "> 
-
-            </table>
+            <ul>
+              {product.map((product,index)=>(
+                <li key={index}>
+                    <ol>
+                      <li>{product.ID}</li>
+                      <li>{product.Name}</li>
+                      <li>{product.Email}</li>
+                      <li>{product.Mobile}</li>
+                    </ol>
+                </li>
+              ))}
+            </ul>
         </div>
     </div>
   )
