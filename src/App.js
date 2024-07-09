dd1aee48964b81646bd62f9c7f2610996fe53504
risnariha <risnariha994563@gmail.com>
2024-07-09 18:19:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState}from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
-function App() {
+const App = () => {
+const[product, setProduct]=useState({
+
+name:"Product",
+price:2000*100,
+productBy:"TechGY",
+
+
+});
+
+const makePayment=(token)=>{
+const body={
+token,
+product
+
+}
+const headers={
+  "Content-Type":"application/json"
+}
+return fetch("http://localhost:5000/payment",{
+method:"POST",
+headers,
+body:JSON.stringify(body)
+
+}).then((response)=>{
+  console.log(response);
+})
+.catch((err)=>{
+console.log(err);
+
+});
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <StripeCheckout name="Buy Product" 
+    amount={product.price}
+    currency="LKR"
+    token={makePayment}
+    stripeKey="pk_test_51Pa2sPAiPGKfCuNVOfPunyGfImCFjlG8U9mjo3MtLnJcxhO0igIS2LqLwfVVTjBUBXv7o2CwCymMX9fNHBYY9YHn00q12iky0F"
+    >
+     <button>Buy Now{product.price/100}</button>
+
+    </StripeCheckout>
+      <h1>Elitz</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
