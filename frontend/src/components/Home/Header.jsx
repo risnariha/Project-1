cb8f62166bbd9895eba9463f26db1d671d7fa6e8
nkrisnaraj <nkrisnaraj007@gmail.com>
2024-07-13 17:09:43 +0530

    changed location of the Header.jsx

---
+++ frontend/src/components/Home/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import logo from '../../../public/Elitez.png'
+import { FaUser, FaInfoCircle, FaUserCircle } from "react-icons/fa";
+export default function Header() {
+
+  const navigate = useNavigate();
+  const handleLogo=()=>{
+    navigate('/');
+  };
+  return (
+    <div className='d-flex w-full bg-white justify-content-between align-items-center'>
+      <div className='d-flex cursor-pointer' onClick={handleLogo}>
+        <div className='align-items-center d-flex'>
+          <img src={logo} alt="" className='h-10 w-10 d-felx ms-3' />
+        </div>
+        <div className='fs-1 fw-bold ms-2 my-2 '>EliteZ</div>
+      </div>
+      <div className='d-flex justify-content-between '>
+        <div className='d-flex px-3'>
+          <Link to ="/Aboutus" className='text-black text-decoration-none'> About
+            <FaInfoCircle className=' mx-2' />
+          </Link>
+        </div>
+        <div className='d-flex px-3'>
+          <Link to ="/Contactus" className='text-black text-decoration-none'> Contact
+            <FaUser className=' mx-2' />
+          </Link>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
