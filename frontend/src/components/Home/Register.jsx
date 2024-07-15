3ad6fdda89d0fb869ffb630381e079d5ad521475
nkrisnaraj <nkrisnaraj007@gmail.com>
2024-07-16 00:24:39 +0530

    Registration request part added successfuly....!

--- frontend/src/components/Home/Register.jsx
+++ frontend/src/components/Home/Register.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './LoginRegister.css';
 import './Login.css';
 import { FaUser, FaLock, FaEnvelope, FaHome, FaCheck, FaRegAddressCard, FaPhone } from "react-icons/fa";
 // import { IoIosArrowDown } from "react-icons/io";
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
   const [userName, setUserName] = useState('');
@@ -13,33 +14,49 @@ const Register = () => {
   const [email, setEmail] = useState('');
   const [address, setAddress] = useState('');
   const [contactNumber, setContactNumber] = useState('');
-  const [password, setPassword] = useState('');
+  const [district, setDistrict] = useState('');
   const [step, setStep] = useState(1);
   const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [messageType, setMessageType] = useState('');
-
+  const districts = [
+    "Ampara", "Anuradhapura", "Badulla", "Batticaloa", "Colombo",
+    "Galle", "Gampaha", "Hambantota", "Jaffna", "Kalutara",
+    "Kandy", "Kegalle", "Kilinochchi", "Kurunegala", "Mannar",
+    "Matale", "Matara", "Monaragala", "Mullaitivu", "Nuwara Eliya",
+    "Polonnaruwa", "Puttalam", "Ratnapura", "Trincomalee", "Vavuniya"
+  ];
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const stepOne = JSON.parse(sessionStorage.getItem('stepOne'));
+    if (stepOne) {
+      const email = stepOne.email;
+      setEmail(email || '');
+      const userType = stepOne.selectedUserType;
+      setSelectedUserType(userType || '');
+    }
+  }, []);
 
   const handleFirstStepSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      // const response = await fetch('http://localhost:8080/backend/api/Company/LoginRegister/checkCompany.php', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json'
-      //   },
-
+      const response = await axios.post('http://localhost:8080/backend/api/Home/emailVerification.php', { email })
+      const data = response.data;
       sessionStorage.setItem('stepOne', JSON.stringify({ email, selectedUserType }))
-      const response = JSON.parse(sessionStorage.getItem('stepOne'))
 
-      if (response) {
+      if (data) {
+        console.log(data.message);
+      }
+      if (data.success) {
+        // console.log('message : ', response.data.message);
 
         setStep(2);
         setMessage('');
+        // console.log('step : ', step);
       } else {
-        setMessage('Company validation failed');
+        setMessage('email validation failed');
         setMessageType('error');
       }
 
@@ -54,28 +71,25 @@ const Register = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8080/backend/api/Home/Login.php', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ companyName, username, email, address, contactNumber, password })
-      });
+      const response = await axios.post('http://localhost:8080/backend/api/Home/register.php',
+        { companyName,shopName,selectedUserType, userName, email, address, contactNumber, district }
+      );
 
-      if (response.ok) {
-        const result = await response.json();
-        if (result.success) {
-          localStorage.setItem('companyName', companyName); // Save company name to local storage
-          setMessage('Registration successful');
+      if (response.data) {
+        console.log(response.data);
+        const data =response.data;
+        if (data.success) {
+          // localStorage.setItem('companyName', companyName); // Save company name to local storage
+          setMessage('Registration request successfully sent');
           setMessageType('success');
-          setTimeout(() => navigate('/dashboard'), 2000);
+          sessionStorage.clear('stepOne');
+          setTimeout(() => navigate('/'), 2000);
         } else {
           setMessage(result.message || 'Registration failed');
           setMessageType('error');
         }
       } else {
-        const errorText = await response.text();
-        console.error('Failed to register company:', errorText);
+        console.log('server error.....')
         setMessage('Server error. Please try again later.');
         setMessageType('error');
       }
@@ -107,29 +121,30 @@ const Register = () => {
         <div className="homeBanner  background-opacity">
           <div className="homePageContainer">
             <div className="homeBannerHeader form-box">
-              
+            {message && <div className={`message ${messageType} option`}>{message}</div>}
               {step === 1 && (
                 <form onSubmit={handleFirstStepSubmit} className='form'>
                   <h2 id='h1'>Registration</h2>
-                  {message && <div className={`message ${messageType}`}>{message}</div>}
+                 
                   {/* <h3>Validation Form</h3> */}
                   <div className="input-box">
                     <input
                       type="email"
                       placeholder="Email"
-                      value={userName}
+                      value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       required
                     />
-                    <FaHome className="icon" />
+                    <FaEnvelope className="icon" />
+
                   </div>
                   <div className="input-box">
 
                     <div className='option'>
-                      <select id="userType" 
-                      value={selectedUserType} 
-                      onChange={(e) => setSelectedUserType(e.target.value)}
-                      className='select'>
+                      <select id="userType"
+                        value={selectedUserType}
+                        onChange={(e) => setSelectedUserType(e.target.value)}
+                        className='select'>
                         <option value="" >  Please choose an option</option>
                         <option value="Company">Company</option>
                         <option value="Shop">Shop</option>
@@ -141,7 +156,7 @@ const Register = () => {
                   <button type="submit" className='submit'>Next</button>
                   <div className="register-link">
 
-                    <span>Already have an account?<Link to="/login"> Login</Link></span>
+                    <span>Already have an account?<Link to="/login" onClick={() => sessionStorage.clear('stepOne')}> Login</Link></span>
 
                   </div>
                 </form>
@@ -159,16 +174,28 @@ const Register = () => {
                     />
                     <FaUser className="icon" />
                   </div>
-                  <div className="input-box">
-                    <input
-                      type="email"
-                      placeholder="Email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      required
-                    />
-                    <FaEnvelope className="icon" />
-                  </div>
+                  {selectedUserType === 'Company' && (
+                    <div className="input-box">
+                      <input
+                        type="text"
+                        placeholder="Company Name"
+                        value={companyName}
+                        onChange={(e) => setCompanyName(e.target.value)}
+                        required
+                      />
+                      <FaHome className="icon" />
+                    </div>)}
+                  {selectedUserType === 'Shop' && (
+                    <div className="input-box">
+                      <input
+                        type="text"
+                        placeholder="Shop Name"
+                        value={shopName}
+                        onChange={(e) => setShopname(e.target.value)}
+                        required
+                      />
+                      <FaHome className="icon" />
+                    </div>)}
                   <div className="input-box">
                     <input
                       type="text"
@@ -189,7 +216,7 @@ const Register = () => {
                     />
                     <FaPhone className="icon" />
                   </div>
-                  <div className="input-box">
+                  {/* <div className="input-box">
                     <input
                       type="password"
                       placeholder="Password"
@@ -198,8 +225,24 @@ const Register = () => {
                       required
                     />
                     <FaLock className="icon" />
+                  </div> */}
+
+                  <div className="input-box">
+                    <div className='option'>
+                      <select
+                        value={district}
+                        className='select'
+                        onChange={(e) => setDistrict(e.target.value)} required>
+                        <option value="" disabled>Select a district</option>
+                        {districts.map((district, index) => (
+                          <option key={index} value={district}>{district}</option>
+                        ))}
+                      </select>
+                    </div>
                   </div>
-                  <button type="submit">Register</button>
+                  <button type="submit" className='submit'>Send Registration Request</button>
+                  <button type="submit" className='mt-2 text-white bg-secondary' onClick={() => setStep(1)}>Back</button>
+
                 </form>
               )}
               <h1>EliteZ</h1>
