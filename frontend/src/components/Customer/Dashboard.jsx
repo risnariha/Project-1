b89e035f6623cba08be4a9ca9523cb224b6c4dc7
nkrisnaraj <nkrisnaraj007@gmail.com>
2024-08-05 02:42:45 +0530

    modified in the search bar options

--- frontend/src/components/Customer/Dashboard.jsx
+++ frontend/src/components/Customer/Dashboard.jsx
@@ -3,12 +3,14 @@ import { Navbar } from './Navbar'
 import ProuductList from './ProuductList'
 import CustomerProfile from './CustomerProfile'
 
-export const Dashboard = ({user,sidebarToggle,setSidebarToggle}) => {
+export const Dashboard = ({user,sidebarToggle,setSidebarToggle,search,setSearch}) => {
   return (
     <div className={`${sidebarToggle? "ml-25 z-n1":""} w-full d-flex flex-column align-items-center`}>
         <Navbar
         sidebarToggle={sidebarToggle}
         setSidebarToggle={setSidebarToggle}
+        search={search}
+        setSearch={setSearch}
         user={user}
         />
         {/* <ProuductList/> */}
