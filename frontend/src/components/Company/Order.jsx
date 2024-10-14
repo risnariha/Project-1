4cee824bdb410570f6845a1f103208fb2f714628
Sarujanan <sarujanang@gmail.com>
2024-10-15 01:12:04 +0530

    add some css

--- frontend/src/components/Company/Order.jsx
+++ frontend/src/components/Company/Order.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { FaTrash, FaEdit, FaTruck,FaHome, FaShippingFast, FaCheck,FaSearch } from "react-icons/fa";
 import { MdOutlineHomeWork } from "react-icons/md";
-// import DatePicker from "react-datepicker"; // Import DatePicker
-// import "react-datepicker/dist/react-datepicker.css";
+ import DatePicker from "react-datepicker"; // Import DatePicker
+ import "react-datepicker/dist/react-datepicker.css";
 import axios from "axios";
 import { Modal, Button } from 'react-bootstrap';
 import { useOutletContext } from "react-router-dom";
@@ -196,17 +196,21 @@ function Order() {
                 <td>{formatDate(order.orderDate)}</td>
                 <td>{currentOrderId === order.orderID ? (
                     <>
+                     <div className="datepicker-wrapper">
                       <DatePicker
                         selected={editingDeliveryDate}
                         onChange={handleDateChange}
                         dateFormat="yyyy/MM/dd"
+                         className="small-datepicker"
                       />
-                      <button onClick={() => saveDeliveryDate(order.orderID)}>Save</button>
+                      </div>
+                      <button className="save-button" onClick={() => saveDeliveryDate(order.orderID)}>Save</button>
                     </>
                   ) : (
                     <>
                       {formatDate(order.deliveryDate)}
                       <button onClick={() => {
+                         className="calendar-button"
                         setCurrentOrderId(order.orderID);
                         setEditingDeliveryDate(new Date(order.deliveryDate)); // Set current delivery date
                       }}>
