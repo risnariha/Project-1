2ec1c61ce2d2b5ed14a80826a77fca5696e31441
nkrisnaraj <nkrisnaraj007@gmail.com>
2024-10-17 20:27:48 +0530

    Changed the background image for home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css';
 
 export const Home = () => {
     return (
-        <div>
+        <div className=''>
             <div className="homeHeader">
 
                 <div className="homeHeaderLinks" >
